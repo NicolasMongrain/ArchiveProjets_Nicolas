d1e7607cf16fe7683a2cab442001298cebf4a260
Nicolas Mongrain <nicolas.mongrain11@gmail.com>
2026-04-27 22:28:44 -0400

    Ajoue d'un viewbox
    Ajoue d'un viewbox et de constellation de plus

--- js/bgIndex.js
+++ js/bgIndex.js
@@ -3,17 +3,17 @@ const glow = document.getElementById("cursor-glow");
 glow.style.background = "radial-gradient(circle, #6AADF4 0%, transparent 70%)";
 
 document.addEventListener("mousemove", (e) => {
-    glow.style.left = e.clientX + "px";
-    glow.style.top = e.clientY + "px";
+  glow.style.left = e.clientX + "px";
+  glow.style.top = e.clientY + "px";
 });
 
-  const lines = document.querySelectorAll(".line");
-  const stars = document.querySelectorAll(".star");
+const lines = document.querySelectorAll(".line");
+const stars = document.querySelectorAll(".star");
 
-  lines.forEach((line, i) => {
-    line.style.animationDelay = `${i * 0.03}s`;
-  });
+lines.forEach((line, i) => {
+  line.style.animationDelay = `${i * 0.03}s`;
+});
 
-  stars.forEach((star, i) => {
-    star.style.animationDelay = `${0.5 + i * 0.01}s`;
-  });
+stars.forEach((star, i) => {
+  star.style.animationDelay = `${0.5 + i * 0.01}s`;
+});
