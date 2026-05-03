9e02911249116510ed5e10fdefa0c0d0a0403eb8
Nicolas Mongrain <nicolas.mongrain11@gmail.com>
2026-05-02 20:51:07 -0400

    Ajoue du JSON pour les compétences

---
+++ src/js/competencesJSON.js
@@ -0,0 +1,172 @@
+export const competences = [
+  //Programmation orienté objet
+  {
+    id: 1,
+    nom: "Java",
+    niveau: "Avancé",
+    image: "../images/competences/logoJava.png",
+    groupe: "Programmation Orienté Objets"
+  },
+  {
+    id: 2,
+    nom: "Python",
+    niveau: "Intermédiaire",
+    image: "../images/competences/logoPython.png",
+    groupe: "Programmation Orienté Objets"
+  },
+  {
+    id: 3,
+    nom: "C#",
+    niveau: "Intermédiaire",
+    image: "../images/competences/logoCSharp.png",
+    groupe: "Programmation Orienté Objets"
+  },
+  // Programmation WEB
+  {
+    id: 4,
+    nom: "HTML",
+    niveau: "Avancé",
+    image: "../images/competences/logoHTML.png",
+    groupe: "Programmation Web"
+  },
+  {
+    id: 5,
+    nom: "CSS",
+    niveau: "Avancé",
+    image: "../images/competences/logoCSS.png",
+    groupe: "Programmation Web"
+  },
+  {
+    id: 6,
+    nom: "TailWindCSS",
+    niveau: "Intermédiaire",
+    image: "../images/competences/logoTailWindCSS.png",
+    groupe: "Programmation Web"
+  },
+  {
+    id: 7,
+    nom: "Javascript",
+    niveau: "Intermédiaire",
+    image: "../images/competences/logoJS.png",
+    groupe: "Programmation Web"
+  },
+  {
+    id: 8,
+    nom: "PHP",
+    niveau: "Intermédiaire",
+    image: "../images/competences/logoPHP.png",
+    groupe: "Programmation Web"
+  },
+  {
+    id: 9,
+    nom: "React",
+    niveau: "En apprentissage",
+    image: "../images/competences/logoReact.png",
+    groupe: "Programmation Web"
+  },
+  //Programmation Assembleur et Script
+  {
+    id: 10,
+    nom: "PEP/8",
+    niveau: "Avancé",
+    image: "../images/competences/logoPep8.png",
+    groupe: "Programmation Assembleur et Script"
+  },
+  {
+    id: 11,
+    nom: "Bash",
+    niveau: "Débutant",
+    image: "../images/competences/logoBash.png",
+    groupe: "Programmation Assembleur et Script"
+  },
+  {
+    id: 12,
+    nom: "PowerShell",
+    niveau: "Débutant",
+    image: "../images/competences/logoPowerShell.png",
+    groupe: "Programmation Assembleur et Script"
+  },
+  // Programmation Database
+  {
+    id: 13,
+    nom: "Oracle Database",
+    niveau: "Intermédiaire",
+    image: "../images/competences/logoOracleDatabase.png",
+    groupe: "Programmation Database"
+  },
+  {
+    id: 14,
+    nom: "SQL",
+    niveau: "Intermédiaire",
+    image: "../images/competences/logoSQL.png",
+    groupe: "Programmation Database"
+  },
+  //Réseau
+  {
+    id: 15,
+    nom: "WireShark",
+    niveau: "Débutant",
+    image: "../images/competences/logoWireShark.png",
+    groupe: "Réseau"
+  },
+  {
+    id: 16,
+    nom: "Cisco Packet Tracer",
+    niveau: "Débutant",
+    image: "../images/competences/logoCiscoPacketTracer.png",
+    groupe: "Réseau"
+  },
+  // Application mobile
+  {
+    id: 17,
+    nom: "Android Studio",
+    niveau: "Intermédiaire",
+    image: "../images/competences/logoAndroidStudio.png",
+    groupe: "Application mobile"
+  },
+  // Création de jeux
+  {
+    id: 18,
+    nom: "Unity",
+    niveau: "Débutant",
+    image: "../images/competences/logoUnity.png",
+    groupe: "Création de jeux"
+  },
+  //Création Artistique
+  {
+    id: 19,
+    nom : "Photoshop",
+    niveau: "Débutant",
+    image: "../images/competences/logoPhotoshop.png",
+    groupe: "Création Artistique"
+  },
+  {
+    id: 20,
+    nom: "MagicaVoxel",
+    niveau: "Avancé",
+    image: "../images/competences/logoMagicaVoxel.png",
+    groupe: "Création Artistique"
+  },
+  {
+    id: 21,
+    nom: "Aseprite",
+    niveau: "Intermédiaire",
+    image: "../images/competences/logoAseprite.png",
+    groupe: "Création Artistique"
+  },
+  //Autres
+  {
+    id: 22,
+    nom: "Vegas Pro",
+    niveau: "Intermédiaire",
+    image: "../images/competences/logoVegasPro.png",
+    groupe: "Autres"
+  },
+  {
+    id:23,
+    nom: "RSLogix",
+    niveau: "Intermédiare",
+    image: "../images/competences/logoRSLogix.png",
+    groupe: "Autres"
+  }
+];
