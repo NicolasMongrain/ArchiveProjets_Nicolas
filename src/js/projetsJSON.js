121144b3a28d61b346c17294328d4f34f8e5a038
Nicolas Mongrain <nicolas.mongrain11@gmail.com>
2026-05-09 15:44:35 -0400

    Create projetsJSON.js
    Début du json des différents projets qui seront sur le site

---
+++ src/js/projetsJSON.js
@@ -0,0 +1,143 @@
+export const projets =
+    [
+        {
+            "id": 1,
+            "nom": "BouffETS",
+            "categorie": ["projet-etude"],
+            "status": "Terminé",
+
+            "images": [
+                "../images/competences/logoJava.png",
+                "../images/competences/logoJava.png",
+                "../images/competences/logoJava.png"
+            ],
+
+            "videos": [
+                //"https://www.youtube.com/watch?v=example1"
+            ],
+
+            "description": "Projet d'étude en équipe de 7 consistant à créer un site web de cuisine ainsi qu'une application mobile. Le but était de faire un réseau social ou les gens peuvent partager leurs recettes et les gens peuvent faire des publications sur ses recettes.",
+
+            "apprentissages": [
+                "Travail d'équipe sur un projet logiciel de grande envergure",
+                "Développement web front-end et intégration d'interfaces utilisateur",
+                "Communication et répartition des tâches dans une équipe",
+                "Utilisation d'outils de gestion de projet et de collaboration",
+                "Conception d'interfaces adaptées aux appareils mobiles",
+                "Organisation et structuration d'un projet logiciel",
+                "Développement d'une application mobile connectée à un service web",
+                "Gestion des contraintes et délais d'un projet scolaire"
+            ],
+
+            "duree": "4 mois",
+
+            "anneeCreation": 2026
+        },
+
+        {
+            "id": 2,
+            "nom": "Simulateur de pathfinding A* et Dijkstra",
+            "categorie": ["vitrine", "projet-personnel"],
+            "status" :"En cours",
+
+            "images": [
+                //"images/pathfinding/grid.png",
+                //"images/pathfinding/algorithm.png"
+            ],
+
+            "videos": [],
+
+            "description": "Application permettant de visualiser les algorithmes de pathfinding A* et Dijkstra sur une grille dynamique. Il est également possible d'utiliser des portails et des murs afin de changer et d'offrir plus de possibilités!",
+
+            "apprentissages": [
+                "Implémentation des algorithmes A* et Dijkstra",
+                "Manipulation de structures de données comme les listes, files de priorité et graphes",
+                "Gestion d'une grille interactive et de la visualisation en temps réel",
+                "Optimisation des performances lors des recherches de chemin",
+                "Conception d'interfaces interactives avec Java Swing",
+                "Visualisation étape par étape d'algorithmes complexes",
+                "Organisation du code dans un projet logiciel plus structuré",
+                "Gestion des obstacles, poids et différents comportements de déplacement",
+                "Débogage de systèmes algorithmiques complexes"
+            ],
+
+            "duree": "20 heures",
+
+            "anneeCreation": 2026
+        },
+
+        {
+            "id": 3,
+            "nom": "Chiffrement par flux XOR",
+            "categorie": ["projet-etude"],
+            "status" :"Terminé",
+
+            "images": [
+
+            ],
+
+            "videos": [],
+
+            "description": "Projet d'étude en assembleur Pep/8 fait en équipe de trois ou le but est de faire le chiffrement et le déchiffrement d'un message par flux XOR. Ce projet utilise également un autre projet d'étude qui est le générateur de nombre speudo-aléatoire :",
+
+            "apprentissages": [
+                "Compréhension du fonctionnement du chiffrement par flux XOR",
+                "Manipulation de données binaires et opérations bit à bit",
+                "Utilisation de l'opérateur XOR pour le chiffrement et le déchiffrement",
+                "Compréhension des concepts de base en cryptographie",
+                "Gestion de chaînes de caractères et de leur conversion en données manipulables",
+                "Développement d'algorithmes en assembleur Pep/8",
+                "Gestion de la mémoire et des registres en bas niveau",
+                "Débogage d'un programme assembleur étape par étape",
+                "Compréhension des limites et vulnérabilités d'un chiffrement simple",
+                "Conception d'un système capable de transformer puis restaurer un message original",
+                "Optimisation de la logique de traitement dans un environnement à faibles ressources",
+                "Travail avec les entrées/sorties dans un programme assembleur"
+            ],
+
+            "duree": "3 semaines",
+
+            "annee": "2025"
+        },
+
+        {
+            "id": 4,
+            "nom": "Générateur de nombre speudo-aléatoire",
+            "categorie": ["projet-etude"],
+            "status" :"Terminé",
+
+            "images": [],
+
+            "videos": [],
+
+            "description": "",
+
+            "apprentissages": [
+                "Développement de générateurs de nombres pseudo-aléatoires en assembleur Pep/8",
+                "Compréhension du fonctionnement des générateurs LCG, Xorshift16 et LFSR",
+                "Manipulation avancée des opérations bit à bit (XOR, AND, décalages, rotations)",
+                "Gestion des nombres signés et des limites des entiers sur 16 bits",
+                "Implémentation d’algorithmes mathématiques en environnement bas niveau",
+                "Débogage de comportements inattendus causés par les bits de signe",
+                "Conception d’algorithmes permettant le calcul de périodes de suites récurrentes",
+                "Création d’outils de test pour automatiser la validation des générateurs",
+                "Optimisation et adaptation d’algorithmes théoriques en assembleur",
+                "Compréhension des registres, de la mémoire et des contraintes d’un processeur simulé",
+                "Implémentation d’un produit modulaire sans dépendre d’opérations haut niveau",
+                "Résolution de problèmes liés aux boucles infinies et aux dépassements de capacité",
+                "Validation expérimentale d’algorithmes à l’aide de jeux de tests",
+                "Analyse des propriétés nécessaires pour obtenir des périodes maximales",
+                "Utilisation et adaptation d’algorithmes cryptographiques et pseudo-aléatoires",
+                "Travail d’équipe sur un projet technique complexe",
+                "Lecture et interprétation d’algorithmes théoriques pour les transformer en code assembleur",
+                "Développement d’une meilleure compréhension du fonctionnement interne des ordinateurs",
+                "Conception de programmes capables de tester automatiquement plusieurs configurations",
+                "Utilisation de l’intelligence artificielle comme outil d’assistance pour générer des cas de test et valider les résultats"
+            ],
+
+            "duree": "3 semaines",
+
+            "annee": "2025"
+
+        }
+    ]
